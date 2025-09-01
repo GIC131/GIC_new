4a32fa55073dc62f6a8aad9df7a7cdbe65fa90e9
Abhi-Max <abhideep0204@gmail.com>
2025-09-02 01:04:49 +0530

    Finale Push

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,7 +3,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // This configuration allows the Next.js Image Optimizer to fetch
+    // images from your backend, for both www and non-www versions of your domain.
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'getinteviewconfidence.com',
+        port: '',
+        pathname: '/uploads/**',
+      },
       {
         protocol: 'https',
         hostname: 'www.getinteviewconfidence.com',
